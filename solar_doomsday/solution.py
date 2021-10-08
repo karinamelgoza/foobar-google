0b38638a39f8febdf002c624ce8336ae7230add0
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-08 12:36:02 -0700

    Adds solution

--- solar_doomsday/solution.py
+++ solar_doomsday/solution.py
@@ -1,2 +1,21 @@
+import math
+
+
 def solution(area):
-    pass
+    panels = []
+    while math.sqrt(area) != 1:
+        panels.append(int(math.sqrt(area))**2)
+        area = area - int(math.sqrt(area))**2
+        if area == 0:
+            break
+
+    panels.append(area)
+
+    if 0 in panels:
+        panels.remove(0)
+
+    return tuple(panels)
+
+
+if __name__ == '__main__':
+    print(solution(12))
