cac866d2b490a138120786dff4d300bd65f6c9b7
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-14 15:02:53 -0700

    Adds solution

--- ion_flux_relabeling.py
+++ ion_flux_relabeling.py
@@ -3,37 +3,30 @@ class Node:
         self.root = root
         self.left = self.right = None
 
-    def PrintTree(self):
-        print(self.root)
-        if self.left:
-            self.left.PrintTree()
-        if self.right:
-            self.right.PrintTree()
 
-    def findval(self, lkpval):
-        if lkpval < self.root:
-            if self.left is None:
-                return str(lkpval)+" Not Found"
-            return self.left.findval(lkpval)
-        elif lkpval > self.root:
-            if self.right is None:
-                return str(lkpval)+" Not Found"
-            return self.right.findval(lkpval)
-        else:
-            print(str(self.root) + ' is found')
+def solution(h, q):
+    nums = list(range(1, 2 ** h))
+    height = h
+    find = q
+    roots = {}
+    results = []
+    if nums[-1] in q:
+        roots[nums[-1]] = -1
 
-
-def postorder(height, nums):
-    if height == 1:
-        return Node(nums.pop())
-    node = Node()
-    node.root = nums.pop()
-    node.right = postorder(height-1, nums)
-    node.left = postorder(height-1, nums)
-    return node
-
-
-height = 5
-tree = postorder(height, list(range(1, 2 ** height)))
-# tree.PrintTree()
-print(tree.findval(21))
+    def postorder(height, nums, find):
+        if height == 1:
+            return Node(nums.pop())
+        node = Node()
+        node.root = nums.pop()
+        node.right = postorder(height-1, nums, find)
+        node.left = postorder(height-1, nums, find)
+        for number in find:
+            if node.left.root == number:
+                roots[node.left.root] = node.root
+            elif node.right.root == number:
+                roots[node.right.root] = node.root
+        return node
+    postorder(height, nums, find)
+    for i in q:
+        results.append(roots[i])
+    return results
